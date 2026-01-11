f6bc7ac1186847200945561b155ccc656e2c327c
Zaira <zairahussain27@gmail.com>
2026-01-11 13:54:11 +0530

    Add SQL analysis for  contract, and tenure

--- churn_analysis.sql
+++ churn_analysis.sql
@@ -8,3 +8,26 @@ SELECT
     2
   ) AS churn_rate
 FROM cleaned_churn_dataset;
+
+SELECT 
+    Contract,
+    COUNT(*) AS total_customers,
+    SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+    ROUND(
+        SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS churn_rate
+FROM cleaned_churn_dataset
+GROUP BY Contract
+ORDER BY churn_rate DESC;
+
+SELECT 
+    tenure_group,
+    COUNT(*) AS total_customers,
+    ROUND(
+        SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS churn_rate
+FROM cleaned_churn_dataset
+GROUP BY tenure_group
+ORDER BY churn_rate DESC;
